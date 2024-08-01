f0bbbf06d3aa65b0138934b53bae4de3082bcc4a
HIBBANQURESHI <156018725+HIBBANQURESHI@users.noreply.github.com>
2024-08-01 21:28:09 +0500

    home

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl"> Software Developer </span>
             <h1 className="h1">
-              Hello I'm <br /> <span> Hibban Qureshi </span>
+              Hello I'm <br /> <span> Hibban </span>
 
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
